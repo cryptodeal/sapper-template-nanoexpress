8b2563e417fdaf49f9364d3df901c68910c1a774
James Deal <jimmydeal16@gmail.com>
2021-01-17 02:27:50 -0600

    implement basic server with websockets. todo: fix static serve middleware

--- src/server.js
+++ src/server.js
@@ -1,17 +1,44 @@
 import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
+import nanoexpress from 'nanoexpress';
+//import staticServe from '@nanoexpress/middleware-static-serve';
+//import compression from 'compression';
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+nanoexpress()
+  .ws('/ws', async (req, res) => {
+    console.log('Connecting...');
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    res.on('connection', (ws) => {
+      console.log('Connected');
+
+      ws.on('message', (msg) => {
+        // eslint-disable-next-line security-node/detect-crlf
+        console.log('Message received', msg);
+        ws.send(msg);
+      });
+      ws.on('close', (code, message) => {
+        // eslint-disable-next-line security-node/detect-crlf
+        console.log('Connection closed', { code, message });
+      });
+    });
+    res.on('upgrade', () => {
+      console.log('Connection upgrade');
+    });
+  })
+  .use(async (req) => {
+    req.headers = {};
+    req.forEach((key, value) => {
+      req.headers[key] = value;
+    });
+  })
+  //.use(compression({ threshold: 0 }))
+  .use(sirv('static', { dev }))
+  .use(sapper.middleware())
+  .any('/*', () => {})
+
+	.listen(3000);
